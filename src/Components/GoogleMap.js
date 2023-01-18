730e95df970968b8d2dff1b718d41276456f18e5
JonathanMitchell1234 <88846650+JonathanMitchell1234@users.noreply.github.com>
2023-01-17 19:09:34 -0500

    environmental variable fix

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -14,7 +14,7 @@ const center = {
 function Map() {
 	const { isLoaded } = useJsApiLoader({
 		id: "google-map-script",
-		googleMapsApiKey: process.env.GoogleMapsAPIKey,
+		googleMapsApiKey: "process.env.GoogleMapsAPIKey",
 	});
 
 	return isLoaded ? (
