1c88c38baa4eb65a695500e22bf3001cd8a5ba2d
JonathanMitchell1234 <88846650+JonathanMitchell1234@users.noreply.github.com>
2023-01-17 20:50:31 -0500

    env test

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -15,8 +15,7 @@ const center = {
 function Map() {
 	const { isLoaded } = useJsApiLoader({
 		id: "google-map-script",
-		googleMapsApiKey: process.env.GoogleMapsAPIkey
-		
+		googleMapsApiKey: process.env.REACT_APP_GoogleMapsAPIkey
 	});
 	console.log(process.env);
 
